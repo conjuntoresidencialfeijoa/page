a07a9306401c11145f5fb515fb4bf7a20d91e925
conjuntoresidencialfeijoa <134105162+conjuntoresidencialfeijoa@users.noreply.github.com>
2023-06-03 19:48:27 -0500

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,13 @@
+/* estilos.css */
+#contador {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: #f1f1f1;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+#contador-numero {
+  font-weight: bold;
+}
